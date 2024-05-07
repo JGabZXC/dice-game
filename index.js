7ff109fa3aaeb71ecbee471edbdb568c022c5583
JGabZXC <114480911+JGabZXC@users.noreply.github.com>
2024-05-07 16:00:15 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var d1 = document.querySelector(".d1");
-var d2 = document.querySelector(".d2");
+var d1 = document.querySelector(".d1"); // DICE 1
+var d2 = document.querySelector(".d2"); // DICE 2
 var player = document.querySelector(".winner");
-
+var p1 = document.querySelector(".p1"); // PLAYER 1
+var p2 = document.querySelector(".p2"); // PLAYER 2
+var count1 = 0;
+var count2 = 0;
 function reroll() {
     var randomNumber1 = Math.floor(Math.random() * 6) + 1;
     var randomNumber2 = Math.floor(Math.random() * 6) + 1;
@@ -13,12 +16,15 @@ function reroll() {
 
     if(randomNumber1 > randomNumber2) {
         player.innerHTML = "Player Wins: Player 1";
+        count1++;
+        p1.innerHTML = "WINS: " + count1;
     } else if(randomNumber1 < randomNumber2) {
         player.innerHTML = "Player Wins: Player 2";
+        count2++;
+        p2.innerHTML = "WINS: " + count2;
     } else {
         player.innerHTML = "DRAW";
     }
-    console.log(randomNumber1);
 }
 
 function reset() {
@@ -27,4 +33,8 @@ function reset() {
     d1.setAttribute("src", img1);
     d2.setAttribute("src", img2);
     player.innerHTML = "Player Wins: ";
+    p1.innerHTML = "WINS: ";
+    p2.innerHTML = "WINS: ";
+    count1 = 0;
+    count2 = 0;
 }
